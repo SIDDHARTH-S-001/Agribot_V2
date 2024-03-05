b92fb47c1781790dc4113b2abe8bd3663aa8c171
SIDDHARTH-S-001 <73553742+SIDDHARTH-S-001@users.noreply.github.com>
2024-03-05 12:31:08 +0530

    Add files via upload

--- agribot_testing_1_description/scripts/esp32_code/esp32_code.ino
+++ agribot_testing_1_description/scripts/esp32_code/esp32_code.ino
@@ -1,4 +1,4 @@
-#include <ESP8266WiFi.h>
+#include <WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 
@@ -7,10 +7,10 @@ const char *password = "Micromax";
 
 AsyncWebServer server(80);
 
-#define pwm_1 D1
-#define dir_1 D2
-#define pwm_2 D5
-#define dir_2 D6
+#define pwm_1 16
+#define dir_1 17
+#define pwm_2 18
+#define dir_2 19
 
 float leftWheelSpeed = 0.0;
 float rightWheelSpeed = 0.0;
@@ -24,7 +24,7 @@ void setup() {
   pinMode(dir_2, OUTPUT);
   pinMode(pwm_1, OUTPUT);
   pinMode(pwm_2, OUTPUT);
-  
+
   Serial.begin(115200);
 
   // Connect to Wi-Fi
@@ -34,66 +34,56 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
 
-  Serial.println("Connected to WiFi");  
+  Serial.println("Connected to WiFi");
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
   // Route for setting wheel speed
-  server.on("/set_speed", HTTP_GET, [](AsyncWebServerRequest *request){
-      if (request->hasParam("wheel") && request->hasParam("speed")) {
-        String wheel = request->getParam("wheel")->value();
-        String speedStr = request->getParam("speed")->value();
-  
-        if (wheel == "right") {
-          rightWheelSpeed = speedStr.toFloat();
-        } else if (wheel == "left") {
-          leftWheelSpeed = speedStr.toFloat();
-        }
-  
-        request->send(200, "text/plain", "Speed set successfully");
-      } else {
-        request->send(400, "text/plain", "Missing parameters");
-      }
-  });
+  server.on("/set_speed", HTTP_GET, [](AsyncWebServerRequest *request) {
+    if (request->hasParam("wheel") && request->hasParam("speed")) {
+      String wheel = request->getParam("wheel")->value();
+      String speedStr = request->getParam("speed")->value();
 
+      if (wheel == "right") {
+        rightWheelSpeed = speedStr.toFloat();
+      } else if (wheel == "left") {
+        leftWheelSpeed = speedStr.toFloat();
+      }
+
+      request->send(200, "text/plain", "Speed set successfully");
+    } else {
+      request->send(400, "text/plain", "Missing parameters");
+    }
+  });
 
   // Start server
   server.begin();
 }
 
 void loop() {
+  Serial.print("Left Wheel Speed: ");
+  Serial.println(leftWheelSpeed);
+  Serial.print("Right Wheel Speed: ");
+  Serial.println(rightWheelSpeed);
 
-  if(leftWheelSpeed >= 0 ){
-    digitalWrite(dir_1, HIGH);   
+  if (leftWheelSpeed >= 0) {
+    digitalWrite(dir_1, HIGH);
     left_dir = "HIGH";
-  }
-
-  else if(leftWheelSpeed < 0){
-    digitalWrite(dir_1, LOW); 
+  } else if (leftWheelSpeed < 0) {
+    digitalWrite(dir_1, LOW);
     left_dir = "LOW";
   }
 
-  else if(rightWheelSpeed >= 0 ){
-    digitalWrite(dir_2, HIGH);   
+  if (rightWheelSpeed >= 0) {
+    digitalWrite(dir_2, HIGH);
     right_dir = "HIGH";
-  }
-
-  else if(rightWheelSpeed < 0){
-    digitalWrite(dir_2, LOW); 
+  } else if (rightWheelSpeed < 0) {
+    digitalWrite(dir_2, LOW);
     right_dir = "LOW";
   }
 
-  // For now not giving full PWM
-  left_pwm = int((abs(leftWheelSpeed)/0.35)*255);
-  right_pwm = int((abs(rightWheelSpeed)/0.35)*255);
-
-  if (left_pwm > 255){
-    left_pwm = 255;
-  }
-
-  if (right_pwm > 255){
-    right_pwm = 255;
-  }
+  left_pwm = int((abs(leftWheelSpeed) / 0.35) * 255);
+  right_pwm = int((abs(rightWheelSpeed) / 0.35) * 255);
 
   analogWrite(pwm_1, left_pwm);
   analogWrite(pwm_2, right_pwm);
