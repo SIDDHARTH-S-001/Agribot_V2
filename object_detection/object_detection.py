95ed3b7ebc52faaef6eae94f3069fac4154b473f
SIDDHARTH-S-001 <73553742+SIDDHARTH-S-001@users.noreply.github.com>
2024-03-22 17:10:30 +0530

    implemented detection of  required classes only

--- object_detection/object_detection.py
+++ object_detection/object_detection.py
@@ -1,16 +1,15 @@
 from ultralytics import YOLO
 import cv2
 import math 
+import time
 
-# start webcam
+# Start webcam
 cap = cv2.VideoCapture(0)
-# cap.set(3, 640)
-# cap.set(4, 480)
 
-# model
+# Model
 model = YOLO("yolov8n.pt")
 
-# object classes
+# Object classes to detect
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
               "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
@@ -23,38 +22,52 @@ classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "trai
               "teddy bear", "hair drier", "toothbrush"
               ]
 
+detectNames = ["bottle", "cup", "sports ball"]
+
+# Variables for FPS calculation
+prev_time = 0
+fps = 0
 
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
 
-    # coordinates
+    # Coordinates
     for r in results:
         boxes = r.boxes
 
         for box in boxes:
-            # bounding box
+            # Bounding box
             x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # Convert to int values
 
-            # confidence
+            # Confidence
             confidence = math.ceil((box.conf[0]*100))/100
-            if confidence >= 0.5:  # Check if confidence is above 0.5
-                # put box in cam
-                cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+            # Class name
+            cls_index = int(box.cls[0])
+            cls = classNames[cls_index]
 
-                # class name
-                cls = str(box.cls[0])
-                print(type(cls))
+            if cls in detectNames:
+                if confidence >= 0.5:  # Check if confidence is above 0.5
+                    # Put box in cam
+                    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)   
+                    
+                    # Object details
+                    org = [x1, y1]
+                    font = cv2.FONT_HERSHEY_SIMPLEX
+                    fontScale = 1
+                    color = (255, 0, 0)
+                    thickness = 2
 
-                # object details
-                org = [x1, y1]
-                font = cv2.FONT_HERSHEY_SIMPLEX
-                fontScale = 1
-                color = (255, 0, 0)
-                thickness = 2
+                    cv2.putText(img, cls, org, font, fontScale, color, thickness)
 
-                cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+    # Calculate FPS
+    current_time = time.time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
+
+    # Display FPS in the top right corner
+    cv2.putText(img, f"FPS: {int(fps)}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Webcam', img)
     if cv2.waitKey(1) == ord('q'):
